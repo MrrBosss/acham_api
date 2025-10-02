d881f00a7694df470ac5a0564e4bdfbe9691639b
Пользователь <abdumuratovabduvali07@gmail.com>
2025-10-02 17:25:37 +0500

    Gender dropdown made

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,19 @@
+from random import choices
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
+    GENDER_CHOICES = [
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other"),
+    ]
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
-    gender = models.CharField(max_length=10, blank=True)
+    gender = models.CharField(max_length=10, blank=True, choices=GENDER_CHOICES)
     region = models.CharField(max_length=50, blank=True)
     language = models.CharField(max_length=5, default="en")
 
